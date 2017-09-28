6d27fdc2d3440814a06a3f633478748f3bf6d4cf
Shlomi Goland <golandsh@hotmail.com>
2017-09-28 13:14:59 +0300

    Fixed url

--- src/shared/base.service.ts
+++ src/shared/base.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class  BaseService {
-   protected UrlBase = "http://localhost:24461" 
+   //protected UrlBase = "http://localhost:24461" 
    //protected UrlBase = "https://dnl-dc-siudext.danel-jobs.co.il/" 
-   //protected UrlBase = "http://192.117.252.138:8082/"
+   protected UrlBase = "http://192.117.252.138:8082/"
 }
